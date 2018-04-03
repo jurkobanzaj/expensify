60a2e673ed2fc7bac3f017eb3a58764066d8fe02
Yuriy Piddubnyak <y.piddubnyak@gmail.com>
2018-04-03 12:01:57 +0300

    with add expense

--- src/components/AddExpencePage.js
+++ src/components/AddExpencePage.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React from 'react';
+import { connect } from 'react-redux'
+import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses'
 
-const AddExpencePage = () => (
+const AddExpencePage = (props) => (
     <div>
-        This is from my add expence component
+        <h1>Add Expense</h1>
+        <ExpenseForm 
+            onSubmit={(expense) => {
+                props.dispatch(addExpense(expense));
+                props.history.push('/'); // redirects after expense were added to main page
+            }}
+        />
     </div>
 );
 
-export default AddExpencePage;
+export default connect()(AddExpencePage);
---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+// const date = new Date();
+const now = moment();
+console.log(now.format());
+
+export default class ExpenseForm extends React.Component {
+    state = {
+        description: '',
+        note: '',
+        amount: '',
+        createdAt: moment(),
+        calendarFocused: false,
+        error: ''
+    };
+
+    onDescriptionChange = (e) => {
+        const description = e.target.value;
+        this.setState(() => ({ description }));
+    };
+
+    onNoteChange = (e) => {
+        const note = e.target.value;
+        this.setState(() => ({ note }));
+    };
+
+    onAmountChange = (e) => {
+        const amount = e.target.value;
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) { // in () - regular expression that looks for format XX.XX 
+            this.setState(() => ({ amount }));
+        }
+    };
+
+    onDateChange = (createdAt) => {
+        if (createdAt) {
+            this.setState(() => ({ createdAt }));
+        }
+    };
+
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calendarFocused: focused }))
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        if (!this.state.description || !this.state.amount) {
+            this.setState(() => ({ error: 'Please provide description and amount' })); // error handler
+        } else {
+            this.setState(() => ({ error: '' }));
+            // clear error 
+            this.props.onSubmit({
+                description: this.state.description,
+                amount: parseFloat(this.state.amount, 10) * 100, // converts string into number
+                createdAt: this.state.createdAt.valueOf(), // converts time stamp 
+                note: this.state.note
+            });
+        };
+    };
+    render() {
+        return(
+            <div>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.onSubmit}>
+                    <input 
+                        type="text" 
+                        placeholder="Description"
+                        autoFocus
+                        value={this.state.description}
+                        onChange={this.onDescriptionChange}
+                    />
+                    <input 
+                        type="text" 
+                        placeholder="Amount"
+                        value={this.state.amount}
+                        onChange={this.onAmountChange}
+                    />
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={() => false}
+                    />
+                    <textarea
+                        placeholder="Add a note for your expense (optional)"
+                        value={this.state.note}
+                        onChange={this.onNoteChange}
+                    >
+                    </textarea>
+                    <button>Add Expense</button>
+                </form>
+            </div>
+        );
+    };
+};
