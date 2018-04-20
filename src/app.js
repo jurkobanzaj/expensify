0af7f7cd65c914cd6f3be3f27562717619489707
Yuriy Piddubnyak <y.piddubnyak@gmail.com>
2018-04-20 14:09:11 +0300

    with package.json and webpack modified for prod

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense, removeExpense, editExpense } from './actions/expenses';
 import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
-
 import './styles/Styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
+
+console.log('test');
 
 // store.dispatch(addExpense({ description: 'Water bill', amount: 600, createdAt: 5000 }));
 // store.dispatch(addExpense({ description: 'Gas bill', amount: 1500, createdAt: 7700 }));
