42be9002713b67e7699f6fb57389009ee77e7e34
Yuriy Piddubnyak <y.piddubnyak@gmail.com>
2018-04-27 19:34:41 +0300

    fully functional

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -6,21 +6,19 @@ import numeral from 'numeral';
 // import { removeExpense } from '../actions/expenses';
 
 export const ExpenseListItem = ({ dispatch, description, amount, createdAt, id }) => ( // {} and items! just like that! 
-    <div>
-        <Link to={`/edit/${id}`} ><h3>{description}</h3></Link> 
-        {
-            // LINK! NOT <A></A> TAG!
-        }
-        <p>
+    <Link className="list-item" to={`/edit/${id}`}>
+        <div>
+            <h3 className="list-item__title">{description}</h3>
+            <span className="list-item__sub-title">
+                {moment(createdAt).format('MMMM Do, YYYY')}
+            </span>
+        </div>
+        <h3 className="list-item__data">
             {numeral(amount / 100).format('$0,0.00')}
-            -
-            {moment(createdAt).format('MMMM Do, YYYY')}
-        </p>
-        {
-        // <button onClick={() => dispatch(removeExpense({ id }))}>Remove</button> 
-        //     //id should be an object! as described in actions!
-        }
-    </div>
+        </h3>
+    </Link> 
+    // <button onClick={() => dispatch(removeExpense({ id }))}>Remove</button> 
+    //     //id should be an object! as described in actions!
 );
 
 // export default connect()(ExpenseListItem);
